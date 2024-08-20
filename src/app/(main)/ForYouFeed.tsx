acc3bedf5070526d1681990947b98263e1955ef5
nikhar savaliya <nikhar663@gmail.com>
2024-08-20 22:36:46 +0530

    added Skeleton while loading posts for Feed page.

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -2,9 +2,11 @@
 
 import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
 import kyInstance from "@/lib/ky";
 import { PostsPage } from "@/lib/types";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { stat } from "fs";
 import { Loader } from "lucide-react";
 
 export default function ForYouFeed() {
@@ -28,8 +30,18 @@ export default function ForYouFeed() {
     getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
   if (status == "pending") {
-    return <Loader className="mx-auto animate-spin" />;
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (status == "success" && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        No one has posted anything yet!
+      </p>
+    );
   }
 
   if (status == "error") {
@@ -39,8 +51,6 @@ export default function ForYouFeed() {
       </p>
     );
   }
-
-  const posts = data?.pages.flatMap((page) => page.posts) || [];
 
   return (
     <InfiniteScrollContainer
