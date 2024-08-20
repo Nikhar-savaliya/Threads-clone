c5a4ea44ceeb5d66fad72d3b0078fde14e8459c3
nikhar savaliya <nikhar663@gmail.com>
2024-08-20 22:28:21 +0530

    added Infinite Scrolling in Feed page

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,22 +1,38 @@
 "use client";
 
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
 import kyInstance from "@/lib/ky";
-import { PostData } from "@/lib/types";
-import { useQuery } from "@tanstack/react-query";
+import { PostsPage } from "@/lib/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Loader } from "lucide-react";
 
 export default function ForYouFeed() {
-  const query = useQuery<PostData[]>({
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: ["post-feed", "for-you"],
-    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/for-you",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  if (query.status == "pending") {
+  if (status == "pending") {
     return <Loader className="mx-auto animate-spin" />;
   }
 
-  if (query.status == "error") {
+  if (status == "error") {
     return (
       <p className="text-center text-muted-foreground">
         Oops! Try again later.
@@ -24,11 +40,17 @@ export default function ForYouFeed() {
     );
   }
 
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
   return (
-    <div className="space-y-3">
-      {query.data.map((post) => {
+    <InfiniteScrollContainer
+      className="space-y-3"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => {
         return <Post key={post.id} post={post} />;
       })}
-    </div>
+      {isFetchingNextPage && <Loader className="mx-auto animate-spin" />}
+    </InfiniteScrollContainer>
   );
 }
